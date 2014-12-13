0861ce3dff0d0be5adbcdc396a1becd0da94defd
Ethan Burns <burns.ethan@gmail.com>
2014-12-13 12:57:37 -0500

    Add benchmarks.
    Here's a benchcmp comparison with go's standard regexp package:
    benchmark                         old ns/op     new ns/op     delta
    BenchmarkRegexpMatchEasy0x32      129           34426         +26586.82%
    BenchmarkRegexpMatchEasy0x1K      332           1074961       +323683.43%
    BenchmarkRegexpMatchEasy1x32      109           33746         +30859.63%
    BenchmarkRegexpMatchEasy1x1K      1034          1039288       +100411.41%
    BenchmarkRegexpMatchMediumx32     201           2288          +1038.31%
    BenchmarkRegexpMatchMediumx1K     72060         1132776       +1471.99%
    BenchmarkRegexpMatchHardx32       3893          944609        +24164.29%
    BenchmarkRegexpMatchHardx1K       114819        45691695      +39694.54%
    
    benchmark                         old MB/s     new MB/s     speedup
    BenchmarkRegexpMatchEasy0x32      246.50       0.93         0.00x
    BenchmarkRegexpMatchEasy0x1K      3083.54      0.95         0.00x
    BenchmarkRegexpMatchEasy1x32      292.74       0.95         0.00x
    BenchmarkRegexpMatchEasy1x1K      989.54       0.99         0.00x
    BenchmarkRegexpMatchMediumx32     4.97         0.44         0.09x
    BenchmarkRegexpMatchMediumx1K     14.21        0.90         0.06x
    BenchmarkRegexpMatchHardx32       8.22         0.03         0.00x
    BenchmarkRegexpMatchHardx1K       8.92         0.02         0.00x
    
    Clearly there's some work to be done ☺

---
+++ re1/re1_bench_test.go
@@ -0,0 +1,65 @@
+// Copied from go/test/bench/go1/regexp_test.go,
+// which has the following notice:
+//
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package re1
+
+import (
+	"math/rand"
+	"testing"
+)
+
+// benchmark based on regexp/exec_test.go
+
+var regexpText []byte
+
+func makeRegexpText(n int) []byte {
+	rand.Seed(0) // For reproducibility.
+	if len(regexpText) >= n {
+		return regexpText[:n]
+	}
+	regexpText = make([]byte, n)
+	for i := range regexpText {
+		if rand.Intn(30) == 0 {
+			regexpText[i] = '\n'
+		} else {
+			regexpText[i] = byte(rand.Intn(0x7E+1-0x20) + 0x20)
+		}
+	}
+	return regexpText
+}
+
+func benchmark(b *testing.B, re string, n int) {
+	r, err := Compile([]rune(re), Options{})
+	if err != nil {
+		panic(err)
+	}
+	t := makeRegexpText(n)
+	rs := sliceRunes([]rune(string(t)))
+	b.ResetTimer()
+	b.SetBytes(int64(n))
+	for i := 0; i < b.N; i++ {
+		if r.Match(rs, 0) != nil {
+			b.Fatal("match!")
+		}
+	}
+}
+
+const (
+	easy0  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+	easy1  = "A[AB]B[BC]C[CD]D[DE]E[EF]F[FG]G[GH]H[HI]I[IJ]J$"
+	medium = "[XYZ]ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+	hard   = "[ -~]*ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+)
+
+func BenchmarkRegexpMatchEasy0x32(b *testing.B)  { benchmark(b, easy0, 32<<0) }
+func BenchmarkRegexpMatchEasy0x1K(b *testing.B)  { benchmark(b, easy0, 1<<10) }
+func BenchmarkRegexpMatchEasy1x32(b *testing.B)  { benchmark(b, easy1, 32<<0) }
+func BenchmarkRegexpMatchEasy1x1K(b *testing.B)  { benchmark(b, easy1, 1<<10) }
+func BenchmarkRegexpMatchMediumx32(b *testing.B) { benchmark(b, medium, 1<<0) }
+func BenchmarkRegexpMatchMediumx1K(b *testing.B) { benchmark(b, medium, 1<<10) }
+func BenchmarkRegexpMatchHardx32(b *testing.B)   { benchmark(b, hard, 32<<0) }
+func BenchmarkRegexpMatchHardx1K(b *testing.B)   { benchmark(b, hard, 1<<10) }
