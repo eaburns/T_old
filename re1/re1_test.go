9a11be4d29d11734af87a24bf05474778c02bbcd
Ethan Burns <burns.ethan@gmail.com>
2014-12-19 07:08:25 -0500

    Break up the tests.
    Same tests, but instead of one big barf of everything, they are broken up into separate tests based on what is being tested. The former version rendered -test.run useless.

--- re1/re1_test.go
+++ re1/re1_test.go
@@ -103,19 +103,8 @@ func TestJustParse(t *testing.T) {
 	}
 }
 
-type regexpTest struct {
-	re, str string
-	want    []string
-	opts    Options
-	from    int64
-}
-
-var (
-	rev         = Options{Reverse: true}
-	lit         = Options{Literal: true}
-	del         = Options{Delimited: true}
-	regexpTests = []regexpTest{
-		// No match.
+func TestNoMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "a", str: "", want: nil},
 		{re: "a", str: "x", want: nil},
 		{re: "a", str: "xyz", want: nil},
@@ -123,8 +112,14 @@ var (
 		{re: "[a]", str: "xyz", want: nil},
 		{re: "[^a]", str: "a", want: nil},
 		{re: ".", str: "\n", want: nil},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
-		// Empty match.
+func TestEmptyMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "", str: "", want: []string{""}},
 		{re: "a*", str: "x", want: []string{""}},
 		{re: "a?", str: "x", want: []string{""}},
@@ -132,29 +127,75 @@ var (
 		{re: "[^a]*", str: "aaa", want: []string{""}},
 		{re: "[^a]*", str: "", want: []string{""}},
 		{re: ".*", str: "", want: []string{""}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestSimpleMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "a", str: "a", want: []string{"a"}},
 		{re: "☺", str: "☺", want: []string{"☺"}},
 		{re: "ab", str: "ab", want: []string{"ab"}},
 		{re: "ab", str: "abcdefg", want: []string{"ab"}},
+		{re: ".", str: "☺", want: []string{"☺"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
+
+func TestOrMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "a|b", str: "a", want: []string{"a"}},
 		{re: "a|b", str: "b", want: []string{"b"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
+
+func TestStarMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "a*", str: "", want: []string{""}},
 		{re: "a*", str: "a", want: []string{"a"}},
 		{re: "a*", str: "aaa", want: []string{"aaa"}},
 		{re: "a*", str: "aaabcd", want: []string{"aaa"}},
 		{re: "☺*", str: "☺☺☹", want: []string{"☺☺"}},
-		{re: "ba+", str: "ba", want: []string{"ba"}},
-		{re: "ba+", str: "baaaaad", want: []string{"baaaaa"}},
-		{re: "ba?d", str: "bd", want: []string{"bd"}},
-		{re: "ba?d", str: "bad", want: []string{"bad"}},
-
 		{re: ".*", str: "abcdefg\n", want: []string{"abcdefg"}},
 		{re: "a.*", str: "abcdefg\n", want: []string{"abcdefg"}},
 		{re: ".*g", str: "abcdefg\n", want: []string{"abcdefg"}},
 		{re: "a.*g", str: "abcdefg\n", want: []string{"abcdefg"}},
 		{re: "a**", str: "aaabcd", want: []string{"aaa"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestPlusMatch(t *testing.T) {
+	tests := []regexpTest{
+		{re: "ba+", str: "ba", want: []string{"ba"}},
+		{re: "ba+", str: "baaaaad", want: []string{"baaaaa"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
+
+func TestQuestionMatch(t *testing.T) {
+	tests := []regexpTest{
+		{re: "ba?d", str: "bd", want: []string{"bd"}},
+		{re: "ba?d", str: "bad", want: []string{"bad"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
+
+func TestSubexprMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "(abc)|(def)", str: "abc", want: []string{"abc", "abc", ""}},
 		{re: "(abc)|(def)", str: "abcdef", want: []string{"abc", "abc", ""}},
 		{re: "(abc)|(def)", str: "def", want: []string{"def", "", "def"}},
@@ -166,7 +207,14 @@ var (
 		{re: "(abc)d|abce", str: "abce", want: []string{"abce", ""}},
 		{re: "abcd|(abc)e", str: "abcd", want: []string{"abcd", ""}},
 		{re: "(☺|☹)*", str: "☺☹☺☹☺☹☺", want: []string{"☺☹☺☹☺☹☺", "☺"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestCharClassMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: `[a]*`, str: `aab`, want: []string{`aa`}},
 		{re: `[abc]*`, str: `abcdefg`, want: []string{`abc`}},
 		{re: `[a-c]*`, str: `abcdefg`, want: []string{`abc`}},
@@ -182,7 +230,14 @@ var (
 		{re: `[^d-f]*`, str: `abcef`, want: []string{`abc`}},
 		{re: `[^^]*`, str: `a^`, want: []string{`a`}},
 		{re: `[^abc]*`, str: "xyz\n", want: []string{`xyz`}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestAnchoredMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "^abc", str: "☺abc", from: 1, want: nil},
 		{re: "^abc", str: "\nabc", from: 1, want: []string{"abc"}},
 		{re: "abc$", str: "abcxyz", want: nil},
@@ -192,7 +247,14 @@ var (
 		{re: "^abc$", str: "☺abc\nxyz", from: 1, want: nil},
 		{re: "^abc$", str: "\nabcxyz", from: 1, want: nil},
 		{re: "^abc$", str: "\nabc\nxyz", from: 1, want: []string{"abc"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestReverseMatch(t *testing.T) {
+	tests := []regexpTest{
 		{opts: rev, re: "a", str: "", want: nil},
 		{opts: rev, re: "a", str: "x", want: nil},
 		{opts: rev, re: "", str: "", want: []string{""}},
@@ -200,7 +262,6 @@ var (
 		{opts: rev, re: "a*", str: "baaaa", want: []string{"aaaa"}},
 		{opts: rev, re: "ba*", str: "baaaa", want: []string{"baaaa"}},
 		{opts: rev, re: "(abc|def)*g", str: "defabcg", want: []string{"defabcg", "def"}},
-
 		{opts: rev, re: "abc$", str: "abc☺", from: 1, want: nil},
 		{opts: rev, re: "abc$", str: "abc\n", from: 1, want: []string{"abc"}},
 		{opts: rev, re: "^abc", str: "xyzabc", want: nil},
@@ -210,28 +271,48 @@ var (
 		{opts: rev, re: "^abc$", str: "xyz\nabc☺", from: 1, want: nil},
 		{opts: rev, re: "^abc$", str: "xyzabc\n", from: 1, want: nil},
 		{opts: rev, re: "^abc$", str: "xyz\nabc\n", from: 1, want: []string{"abc"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestLiteralMatch(t *testing.T) {
+	tests := []regexpTest{
 		{opts: lit, re: "a", str: "", want: nil},
 		{opts: lit, re: "a", str: "x", want: nil},
 		{opts: lit, re: "", str: "", want: []string{""}},
 		{opts: lit, re: "[abc]()*?+.", str: "[abc]()*?+.", want: []string{"[abc]()*?+."}},
 		{opts: lit, re: "[abc]()*?+.", str: "[abc]()*?+.☺", want: []string{"[abc]()*?+."}},
 		{opts: lit, re: "Hello, 世界", str: "Hello, 世界!!!!", want: []string{"Hello, 世界"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestDelineatedMatch(t *testing.T) {
+	tests := []regexpTest{
 		{opts: del, re: "/abc", str: "abc", want: []string{"abc"}},
 		{opts: del, re: "/abc/", str: "abc", want: []string{"abc"}},
 		{opts: del, re: "/abc/def", str: "abc", want: []string{"abc"}},
 		{opts: del, re: `/abc\//def`, str: "abc/", want: []string{"abc/"}},
 		{opts: del, re: `/[abc\/]*/def`, str: "abc/", want: []string{"abc/"}},
 		{opts: del, re: `/(.*),\n/def`, str: "hi,\n", want: []string{"hi,\n", "hi"}},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
+func TestMultiOptionMatch(t *testing.T) {
+	tests := []regexpTest{
 		{
 			opts: Options{Literal: true, Reverse: true},
 			re:   "[abc]()*?+.",
 			str:  "☺[abc]()*?+.",
 			want: []string{"[abc]()*?+."},
 		},
-
 		{
 			// Not sure why, but might as well make sure it works.
 			opts: Options{Literal: true, Delimited: true},
@@ -239,8 +320,14 @@ var (
 			str:  "[abc]()*?+.",
 			want: []string{"[abc]()*?+."},
 		},
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
 
-		// Next match.
+func TestNextMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "abc", str: "xyzabc", want: []string{"abc"}},
 		{re: "abc", str: "xyzabc", from: 1, want: []string{"abc"}},
 		{re: "abc", str: "xyzabc", from: 2, want: []string{"abc"}},
@@ -260,7 +347,14 @@ var (
 		{re: ".+", str: "\n\n\n", want: nil},
 		{re: ".+", str: "\n\naa", want: []string{"aa"}},
 		{re: ".+", str: "\n\naa\n", want: []string{"aa"}},
-		// Wrap.
+	}
+	for _, test := range tests {
+		test.run(t)
+	}
+}
+
+func TestWrapMatch(t *testing.T) {
+	tests := []regexpTest{
 		{re: "abc", str: "abcxyz", from: 3, want: []string{"abc"}},
 		{re: "abc", str: "abcxyz", from: 6, want: []string{"abc"}},
 		{re: "abc", str: "xyzabc", from: 4, want: []string{"abc"}},
@@ -268,34 +362,8 @@ var (
 		{re: "^abc|def$", str: "☺abc\ndef", from: 7, want: []string{"def"}},
 		{opts: rev, re: "abc", str: "abcdef", from: 5, want: []string{"abc"}},
 	}
-)
-
-func TestMatch(t *testing.T) {
-	for _, test := range regexpTests {
-		re, err := Compile([]rune(test.re), test.opts)
-		if err != nil {
-			t.Fatalf(`Compile("%s", %+v)=%v, want nil`, test.re, test.opts, err)
-		}
-		str := test.str
-		if test.opts.Reverse {
-			str = reverse(test.str)
-		}
-		es := re.Match(sliceRunes([]rune(str)), test.from)
-		ms := matches(test.str, es, test.opts.Reverse)
-		if (es == nil && test.want == nil) ||
-			(len(es) == len(test.want) && reflect.DeepEqual(ms, test.want)) {
-			continue
-		}
-		got := "<nil>"
-		if es != nil {
-			got = fmt.Sprintf("%v (%v)", es, ms)
-		}
-		want := "<nil>"
-		if test.want != nil {
-			want = fmt.Sprintf("%v", test.want)
-		}
-		t.Errorf(`Compile("%s", %+v).Match("%s", %d)=%s, want %s`,
-			test.re, test.opts, test.str, test.from, got, want)
+	for _, test := range tests {
+		test.run(t)
 	}
 }
 
@@ -324,6 +392,46 @@ type sliceRunes []rune
 
 func (s sliceRunes) Rune(i int64) rune { return s[i] }
 func (s sliceRunes) Size() int64       { return int64(len(s)) }
+
+var (
+	rev = Options{Reverse: true}
+	lit = Options{Literal: true}
+	del = Options{Delimited: true}
+)
+
+type regexpTest struct {
+	re, str string
+	want    []string
+	opts    Options
+	from    int64
+}
+
+func (test *regexpTest) run(t *testing.T) {
+	re, err := Compile([]rune(test.re), test.opts)
+	if err != nil {
+		t.Fatalf(`Compile("%s", %+v)=%v, want nil`, test.re, test.opts, err)
+	}
+	str := test.str
+	if test.opts.Reverse {
+		str = reverse(test.str)
+	}
+	es := re.Match(sliceRunes([]rune(str)), test.from)
+	ms := matches(test.str, es, test.opts.Reverse)
+	if (es == nil && test.want == nil) ||
+		(len(es) == len(test.want) && reflect.DeepEqual(ms, test.want)) {
+		return
+	}
+	got := "<nil>"
+	if es != nil {
+		got = fmt.Sprintf("%v (%v)", es, ms)
+	}
+	want := "<nil>"
+	if test.want != nil {
+		want = fmt.Sprintf("%v", test.want)
+	}
+	t.Errorf(`Compile("%s", %+v).Match("%s", %d)=%s, want %s`,
+		test.re, test.opts, test.str, test.from, got, want)
+}
 
 func matches(str string, es [][2]int64, rev bool) []string {
 	rs := []rune(str)
