c50ad885d69380ef0eac10e8e06bfce833b3bb20
Ethan Burns <burns.ethan@gmail.com>
2014-12-30 07:05:52 -0500

    Update the re1 package doc.
    It now describes the regexp(7) variant that it will implement.

--- re1/re1.go
+++ re1/re1.go
@@ -1,11 +1,10 @@
 /*
-Package re1 is an implementation of Plan 9 regular expressions.
+Package re1 is an implementation of a variant of Plan 9 regular expressions.
 Plan 9 regular expressions are defined in the regexp(7) man page,
 which can be found at http://swtch.com/plan9port/man/man7/regexp.html.
+The following text is from regexp(7), modified to describe the re1 variant.
 
-It reads:
-
-"A regular expression specifies a set of strings of characters. A member of this set of strings is said to be matched by the regular expression. In many applications a delimiter character, commonly /, bounds a regular expression. In the following specification for regular expressions the word ‘character’ means any character (rune) but newline.
+A regular expression specifies a set of strings of characters. A member of this set of strings is said to be matched by the regular expression. In many applications a delimiter character, commonly /, bounds a regular expression. In the following specification for regular expressions the word ‘character’ means any character (rune) but newline.
 
 The syntax for a regular expression e0 is
 	e3:    literal | charclass | '.' | '^' | '$' | '(' e0 ')'
@@ -13,9 +12,9 @@ The syntax for a regular expression e0 is
 	REP:   '*' | '+' | '?'
 	e1:    e2 | e1 e2
 	e0:    e1 | e0 '|' e1
-A literal is any non-metacharacter, or a metacharacter (one of .*+?[]()|\^$), or the delimiter preceded by \.
+A literal is any non-metacharacter, or a metacharacter (one of .*+?[]()|\^$) or the delimiter or the letter n preceded by \. An exception is made if the delimiter is a metacharacter; in that case, when preceeded by \ it is interpreted as its meta form. A literal delimiter can always be matched using a charclass (see below). \n is a literal newline.
 
-A charclass is a nonempty string s bracketed [s] (or [^s]); it matches any character in (or not in) s. A negated character class never matches newline. A substring a−b, with a and b in ascending order, stands for the inclusive range of characters between a and b. In s, the metacharacters −, ], an initial ^, and the regular expression delimiter must be preceded by a \; other metacharacters have no special meaning and may appear unescaped.
+A charclass is a nonempty string s bracketed [s] (or [^s]); it matches any character in (or not in) s. A negated character class never matches newline. A substring a−b, with a and b in ascending order, stands for the inclusive range of characters between a and b. In s, the metacharacters −, ], and an initial ^ must be preceded by a \; other metacharacters including the regular expression delimiter have no special meaning and may appear unescaped.
 
 A . matches any character.
 
@@ -27,9 +26,7 @@ A concatenated regular expression, e1e2, matches a match to e1 followed by a mat
 
 An alternative regular expression, e0|e1, matches either a match to e0 or a match to e1.
 
-A match to any part of a regular expression extends as far as possible without preventing a match to the remainder of the regular expression."
-
-In addition, re1 supports literal newline matching via \n.
+A match to any part of a regular expression extends as far as possible without preventing a match to the remainder of the regular expression.
 */
 package re1
 
