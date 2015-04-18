c6974ece4bc9c816bd99d01fd6e589ecf65ce8b1
Ethan Burns <burns.ethan@gmail.com>
2015-04-18 08:00:04 -0400

    Add io-package-style Reader and Writer interfaces.

---
+++ edit/runes/buffer.go
@@ -0,0 +1,367 @@
+package runes
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+// RuneBytes is the number of bytes in Go's rune type.
+const runeBytes = 4
+
+// A Buffer is an unbounded rune buffer backed by a file.
+type Buffer struct {
+	// F is the file that backs the buffer. It is created lazily.
+	f ReaderWriterAt
+	// BlockSize is the maximum number of runes in a block.
+	blockSize int
+	// Blocks contains all blocks of the buffer in order.
+	// Free contains blocks that are free to be re-allocated.
+	blocks, free []block
+	// End is the byte offset of the end of the backing file.
+	end int64
+
+	// Cache is the index of the block whose data is currently cached.
+	cached int
+	// Cached0 is the address of the first rune in the cached block.
+	cached0 int64
+	// Cache is the cached data.
+	cache []rune
+	// Dirty tracks whether the cached data has changed since it was read.
+	dirty bool
+
+	// Size is the number of runes in the buffer.
+	size int64
+}
+
+// A ReaderWriterAt implements the io.ReaderAt and io.WriterAt interfaces.
+type ReaderWriterAt interface {
+	io.ReaderAt
+	io.WriterAt
+}
+
+// A block describes a portion of the buffer and its location in the backing file.
+type block struct {
+	// Start is the byte offset of the block in the file.
+	start int64
+	// N is the number of runes in the block.
+	n int
+}
+
+// NewBuffer returns a new, empty buffer.
+// No more than blockSize runes are cached in memory.
+func NewBuffer(blockSize int) *Buffer {
+	return &Buffer{
+		blockSize: blockSize,
+		cached:    -1,
+		cache:     make([]rune, blockSize),
+	}
+}
+
+// NewBufferReaderWriterAt is like NewBuffer but uses
+// the given ReaderWriterAt as its backing store.
+// If the ReaderWriterAt implements io.Closer, it is closed when the buffer is closed.
+// If the ReaderWriterAt is an *os.File, the file is removed when the buffer is closed.
+func NewBufferReaderWriterAt(blockSize int, f ReaderWriterAt) *Buffer {
+	b := NewBuffer(blockSize)
+	b.f = f
+	return b
+}
+
+// Close closes the buffer and removes it's backing file.
+func (b *Buffer) Close() error {
+	b.cache = nil
+	switch f := b.f.(type) {
+	case *os.File:
+		path := f.Name()
+		if err := f.Close(); err != nil {
+			return err
+		}
+		return os.Remove(path)
+	case io.Closer:
+		return f.Close()
+	default:
+		return nil
+	}
+}
+
+// Size returns the number of runes in the buffer.
+func (b *Buffer) Size() int64 { return b.size }
+
+// Rune returns the rune at the given offset.
+// If the rune is out of range it panics.
+// If there is an error reading, it panics a RuneReadError containing the error.
+func (b *Buffer) Rune(offs int64) (rune, error) {
+	if offs < 0 || offs > b.Size() {
+		panic("rune index out of bounds")
+	}
+	if q0 := b.cached0; q0 <= offs && offs < q0+int64(b.blocks[b.cached].n) {
+		return b.cache[offs-q0], nil
+	}
+	i, q0 := b.blockAt(offs)
+	if _, err := b.get(i); err != nil {
+		return -1, err
+	}
+	return b.cache[offs-q0], nil
+}
+
+// Read reads runes from the buffer beginning at a given offset.
+// It is an error to read out of the range of the buffer.
+func (b *Buffer) Read(n int, offs int64) ([]rune, error) {
+	if n < 0 {
+		panic("bad count: " + strconv.Itoa(n))
+	}
+	if offs < 0 || offs+int64(n) > b.Size() {
+		return nil, errors.New("invalid offset: " + strconv.FormatInt(offs, 10))
+	}
+	rs := make([]rune, n)
+	for n > 0 {
+		if offs == b.Size() {
+			panic("impossible read")
+		}
+		i, q0 := b.blockAt(offs)
+		blk, err := b.get(i)
+		if err != nil {
+			if err == io.EOF {
+				err = io.ErrUnexpectedEOF
+			}
+			return nil, err
+		}
+		o := int(offs - q0)
+		m := copy(rs[len(rs)-n:], b.cache[o:blk.n])
+		n -= m
+		offs += int64(m)
+	}
+	return rs, nil
+}
+
+// Insert inserts runes into the buffer at the given offset.
+// It is an error to insert at a point than is out of the range of the buffer.
+func (b *Buffer) Insert(rs []rune, offs int64) error {
+	if offs < 0 || offs > b.Size() {
+		return errors.New("invalid offset: " + strconv.FormatInt(offs, 10))
+	}
+	for len(rs) > 0 {
+		i, q0 := b.blockAt(offs)
+		blk, err := b.get(i)
+		if err != nil {
+			return err
+		}
+		m := b.blockSize - blk.n
+		if m == 0 {
+			if i, err = b.insertAt(offs); err != nil {
+				return err
+			}
+			if blk, err = b.get(i); err != nil {
+				return err
+			}
+			q0 = offs
+			m = b.blockSize
+		}
+		if m > len(rs) {
+			m = len(rs)
+		}
+		o := int(offs - q0)
+		copy(b.cache[o+m:], b.cache[o:blk.n])
+		copy(b.cache[o:], rs[:m])
+		b.dirty = true
+		rs = rs[m:]
+		blk.n += m
+		b.size += int64(m)
+		offs += int64(m)
+	}
+	return nil
+}
+
+// Delete deletes runes from the buffer starting at the given offset.
+// It is an error to delete out of the range of the buffer.
+func (b *Buffer) Delete(n, offs int64) error {
+	if n < 0 {
+		panic("bad count: " + strconv.FormatInt(n, 10))
+	}
+	if offs < 0 || offs+n > b.Size() {
+		return errors.New("invalid offset: " + strconv.FormatInt(offs, 10))
+	}
+	for n > 0 {
+		i, q0 := b.blockAt(offs)
+		blk, err := b.get(i)
+		if err != nil {
+			return err
+		}
+		o := int(offs - q0)
+		m := blk.n - o
+		if int64(m) > n {
+			m = int(n)
+		}
+		if o == 0 && n >= int64(blk.n) {
+			// Remove the entire block.
+			b.freeBlock(*blk)
+			b.blocks = append(b.blocks[:i], b.blocks[i+1:]...)
+			b.cached = -1
+		} else {
+			// Remove a portion of the block.
+			copy(b.cache[o:], b.cache[o+m:])
+			b.dirty = true
+			blk.n -= m
+		}
+		n -= int64(m)
+		b.size -= int64(m)
+	}
+	return nil
+}
+
+func (b *Buffer) allocBlock() block {
+	if l := len(b.free); l > 0 {
+		blk := b.free[l-1]
+		b.free = b.free[:l-1]
+		return blk
+	}
+	blk := block{start: b.end}
+	b.end += int64(b.blockSize * runeBytes)
+	return blk
+}
+
+func (b *Buffer) freeBlock(blk block) {
+	b.free = append(b.free, block{start: blk.start})
+}
+
+// BlockAt returns the index and start address of the block containing the address.
+// If the address is one beyond the end of the file, a new block is allocated.
+// BlockAt panics if the address is negative or more than one past the end.
+func (b *Buffer) blockAt(at int64) (int, int64) {
+	if at < 0 || at > b.Size() {
+		panic("invalid offset: " + strconv.FormatInt(at, 10))
+	}
+	if at == b.Size() {
+		i := len(b.blocks)
+		blk := b.allocBlock()
+		b.blocks = append(b.blocks[:i], append([]block{blk}, b.blocks[i:]...)...)
+		return i, at
+	}
+	var q0 int64
+	for i, blk := range b.blocks {
+		if q0 <= at && at < q0+int64(blk.n) {
+			return i, q0
+		}
+		q0 += int64(blk.n)
+	}
+	panic("impossible")
+}
+
+// insertAt inserts a block at the address and returns the new block's index.
+// If a block contains the address then it is split.
+func (b *Buffer) insertAt(at int64) (int, error) {
+	i, q0 := b.blockAt(at)
+	o := int(at - q0)
+	blk := b.blocks[i]
+	if at == q0 {
+		// Adding immediately before blk, no need to split.
+		nblk := b.allocBlock()
+		b.blocks = append(b.blocks[:i], append([]block{nblk}, b.blocks[i:]...)...)
+		if b.cached == i {
+			b.cached = i + 1
+		}
+		return i, nil
+	}
+
+	// Splitting blk.
+	// Make sure it's both on disk and in the cache.
+	if b.cached == i && b.dirty {
+		if err := b.put(); err != nil {
+			return -1, err
+		}
+	} else if _, err := b.get(i); err != nil {
+		return -1, err
+	}
+
+	// Resize blk.
+	b.blocks[i].n = int(o)
+
+	// Insert the new, empty block.
+	nblk := b.allocBlock()
+	b.blocks = append(b.blocks[:i+1], append([]block{nblk}, b.blocks[i+1:]...)...)
+
+	// Allocate a block for the second half of blk and set it as the cache.
+	// The next put will write it out.
+	nblk = b.allocBlock()
+	b.blocks = append(b.blocks[:i+2], append([]block{nblk}, b.blocks[i+2:]...)...)
+	b.blocks[i+2].n = blk.n - o
+	copy(b.cache, b.cache[o:])
+	b.cached = i + 2
+	b.dirty = true
+
+	return i + 1, nil
+}
+
+// File returns an *os.File, creating a new file if one is not created yet.
+func (b *Buffer) file() (ReaderWriterAt, error) {
+	if b.f == nil {
+		f, err := ioutil.TempFile(os.TempDir(), "edit")
+		if err != nil {
+			return nil, err
+		}
+		b.f = f
+	}
+	return b.f, nil
+}
+
+// Put writes the cached block back to the file.
+func (b *Buffer) put() error {
+	if b.cached < 0 || !b.dirty || len(b.cache) == 0 {
+		return nil
+	}
+	blk := b.blocks[b.cached]
+	f, err := b.file()
+	if err != nil {
+		return err
+	}
+	bs := make([]byte, blk.n*runeBytes)
+	for i, r := range b.cache[:blk.n] {
+		binary.LittleEndian.PutUint32(bs[i*runeBytes:], uint32(r))
+	}
+	if _, err := f.WriteAt(bs, blk.start); err != nil {
+		return err
+	}
+	b.dirty = false
+	return nil
+}
+
+// Get loads the cache with the data from the block at the given index,
+// returning a pointer to it.
+func (b *Buffer) get(i int) (*block, error) {
+	if b.cached == i {
+		return &b.blocks[i], nil
+	}
+	if err := b.put(); err != nil {
+		return nil, err
+	}
+
+	blk := b.blocks[i]
+	f, err := b.file()
+	if err != nil {
+		return nil, err
+	}
+	bs := make([]byte, blk.n*runeBytes)
+	if _, err := f.ReadAt(bs, blk.start); err != nil {
+		if err == io.EOF {
+			panic("unexpected EOF")
+		}
+		return nil, err
+	}
+	j := 0
+	for len(bs) > 0 {
+		b.cache[j] = rune(binary.LittleEndian.Uint32(bs))
+		bs = bs[runeBytes:]
+		j++
+	}
+	b.cached = i
+	b.dirty = false
+	b.cached0 = 0
+	for j := 0; j < i; j++ {
+		b.cached0 += int64(b.blocks[j].n)
+	}
+	return &b.blocks[i], nil
+}
--- edit/runes/runes_test.go
+++ edit/runes/runes_test.go
@@ -3,357 +3,70 @@
 package runes
 
 import (
-	"errors"
+	"io"
 	"reflect"
-	"regexp"
 	"testing"
 )
 
-const testBlockSize = 8
+var (
+	helloWorldTestRunes = []rune("Hello, World! αβξ")
+	helloWorldReadTests = readTests{
+		{0, ""},
+		{5, "Hello"},
+		{2, ", "},
+		{6, "World!"},
+		{0, ""},
+		{1, " "},
+		{100, "αβξ"},
+	}
+)
 
-func TestRunesRune(t *testing.T) {
-	rs := []rune("Hello, 世界!")
-	b := NewBuffer(testBlockSize)
-	defer b.Close()
-	if err := b.Insert(rs, 0); err != nil {
-		t.Fatalf(`b.Insert("%s", 0)=%v, want nil`, string(rs), err)
-	}
-	for i, want := range rs {
-		if got, err := b.Rune(int64(i)); err != nil || got != want {
-			t.Errorf("b.Rune(%d)=%v,%v, want %v,nil", i, got, err, want)
-		}
-	}
+func TestSliceReader(t *testing.T) {
+	r := &SliceReader{helloWorldTestRunes}
+	helloWorldReadTests.run(t, r)
 }
 
-func TestRead(t *testing.T) {
-	b := makeTestBytes(t)
-	defer b.Close()
-	tests := []struct {
-		n    int
-		offs int64
-		want string
-		err  string
-	}{
-		{n: 1, offs: 27, err: "invalid offset"},
-		{n: 1, offs: 28, err: "invalid offset"},
-		{n: 1, offs: -1, err: "invalid offset"},
-		{n: 1, offs: -2, err: "invalid offset"},
+// TestLimitedReaderBigReader tests the LimitedReader
+// where the underlying reader is bigger than the limit.
+func TestLimitedReaderBigReader(t *testing.T) {
+	left := int64(len(helloWorldTestRunes))
+	bigRunes := make([]rune, left*10)
+	copy(bigRunes, helloWorldTestRunes)
+	r := &LimitedReader{Reader: &SliceReader{bigRunes}, N: left}
+	helloWorldReadTests.run(t, r)
+}
 
-		{n: 0, offs: 0, want: ""},
-		{n: 1, offs: 0, want: "0"},
-		{n: 1, offs: 26, want: "Z"},
-		{n: 8, offs: 19, want: "STUVWXYZ"},
-		{n: 11, offs: 8, want: "abcd!@#efgh"},
-		{n: 7, offs: 12, want: "!@#efgh"},
-		{n: 6, offs: 13, want: "@#efgh"},
-		{n: 5, offs: 13, want: "@#efg"},
-		{n: 4, offs: 15, want: "efgh"},
-		{n: 27, offs: 0, want: "01234567abcd!@#efghSTUVWXYZ"},
-	}
+// TestLimitedReaderSmallReader tests the LimitedReader
+// where the underlying reader is smaller than the limit.
+func TestLimitedReaderSmallReader(t *testing.T) {
+	// Chop off the last 3 runes,
+	// and the last readTest element.
+	rs := helloWorldTestRunes[:len(helloWorldTestRunes)-3]
+	tests := helloWorldReadTests[:len(helloWorldReadTests)-1]
+
+	left := int64(len(helloWorldTestRunes))
+	r := &LimitedReader{Reader: &SliceReader{rs}, N: left}
+	tests.run(t, r)
+}
+
+type readTests []struct {
+	n    int
+	want string
+}
+
+func (tests readTests) run(t *testing.T, r Reader) {
 	for _, test := range tests {
-		rs, err := b.Read(test.n, test.offs)
-		if str := string(rs); !errMatch(test.err, err) || str != test.want {
-			t.Errorf("ReadAt(%v, %v)=%q,%v, want %q,%v", test.n, test.offs, str, err, test.want, test.err)
+		w := []rune(test.want)
+		p := make([]rune, test.n)
+		m, err := r.Read(p)
+		if m != len(w) || !reflect.DeepEqual(p[:m], w) || (err != nil && err != io.EOF) {
+			t.Errorf("Read(len=%d)=%d,%v; %q want %d,<nil>; %q",
+				test.n, m, err, string(p[:m]), len(w), test.want)
+			return
 		}
 	}
-}
-
-func TestEmptyReadAtEOF(t *testing.T) {
-	b := NewBuffer(testBlockSize)
-	defer b.Close()
-
-	if rs, err := b.Read(0, 0); len(rs) != 0 || err != nil {
-		t.Errorf("empty buffer Read(0, 0)=%q,%v, want {},nil", rs, err)
-	}
-
-	str := "Hello, World!"
-	if err := b.Insert([]rune(str), 0); err != nil {
-		t.Fatalf("insert(%v, 0)=%v, want nil", str, err)
-	}
-
-	if rs, err := b.Read(0, 1); len(rs) != 0 || err != nil {
-		t.Errorf("Read(0, 1)=%q,%v, want {},nil", rs, err)
-	}
-
-	l := len(str)
-	if err := b.Delete(int64(l), 0); err != nil {
-		t.Fatalf("delete(%v, 0)=%v, want nil", l, err)
-	}
-	if s := b.Size(); s != 0 {
-		t.Fatalf("b.Size()=%d, want 0", s)
-	}
-
-	// The buffer should be empty, but we still don't want EOF when reading 0 bytes.
-	if rs, err := b.Read(0, 0); len(rs) != 0 || err != nil {
-		t.Errorf("deleted buffer Read(0, 0)=%q,%v, want {},nil", rs, err)
-	}
-}
-
-func TestInsert(t *testing.T) {
-	tests := []struct {
-		init, add string
-		at        int64
-		want      string
-		err       string
-	}{
-		{init: "", add: "0", at: -1, err: "invalid offset"},
-		{init: "", add: "0", at: 1, err: "invalid offset"},
-		{init: "0", add: "1", at: 2, err: "invalid offset"},
-
-		{init: "", add: "", at: 0, want: ""},
-		{init: "", add: "0", at: 0, want: "0"},
-		{init: "", add: "012", at: 0, want: "012"},
-		{init: "", add: "01234567", at: 0, want: "01234567"},
-		{init: "", add: "012345670", at: 0, want: "012345670"},
-		{init: "", add: "0123456701234567", at: 0, want: "0123456701234567"},
-		{init: "1", add: "0", at: 0, want: "01"},
-		{init: "2", add: "01", at: 0, want: "012"},
-		{init: "0", add: "01234567", at: 0, want: "012345670"},
-		{init: "01234567", add: "01234567", at: 0, want: "0123456701234567"},
-		{init: "01234567", add: "01234567", at: 8, want: "0123456701234567"},
-		{init: "0123456701234567", add: "01234567", at: 8, want: "012345670123456701234567"},
-		{init: "02", add: "1", at: 1, want: "012"},
-		{init: "07", add: "123456", at: 1, want: "01234567"},
-		{init: "00", add: "1234567", at: 1, want: "012345670"},
-		{init: "01234567", add: "abc", at: 1, want: "0abc1234567"},
-		{init: "01234567", add: "abc", at: 2, want: "01abc234567"},
-		{init: "01234567", add: "abc", at: 3, want: "012abc34567"},
-		{init: "01234567", add: "abc", at: 4, want: "0123abc4567"},
-		{init: "01234567", add: "abc", at: 5, want: "01234abc567"},
-		{init: "01234567", add: "abc", at: 6, want: "012345abc67"},
-		{init: "01234567", add: "abc", at: 7, want: "0123456abc7"},
-		{init: "01234567", add: "abc", at: 8, want: "01234567abc"},
-		{init: "01234567", add: "abcdefgh", at: 4, want: "0123abcdefgh4567"},
-		{init: "01234567", add: "abcdefghSTUVWXYZ", at: 4, want: "0123abcdefghSTUVWXYZ4567"},
-		{init: "0123456701234567", add: "abcdefgh", at: 8, want: "01234567abcdefgh01234567"},
-	}
-	for _, test := range tests {
-		b := NewBuffer(testBlockSize)
-		defer b.Close()
-		if len(test.init) > 0 {
-			if err := b.Insert([]rune(test.init), 0); err != nil {
-				t.Errorf("%+v init failed: insert(%v, 0)=%v, want nil", test, test.init, err)
-				continue
-			}
-		}
-		err := b.Insert([]rune(test.add), test.at)
-		if !errMatch(test.err, err) {
-			t.Errorf("%+v add failed: insert(%v, %v)=%v, want %v", test, test.add, test.at, err, test.err)
-			continue
-		}
-		if test.err != "" {
-			continue
-		}
-		if s := readAll(b); s != test.want || err != nil {
-			t.Errorf("%+v read failed: readAll(·)=%v,%v, want %v,nil", test, s, err, test.want)
-			continue
-		}
-	}
-}
-
-func TestDelete(t *testing.T) {
-	tests := []struct {
-		n, at int64
-		want  string
-		err   string
-	}{
-		{n: 1, at: 27, err: "invalid offset"},
-		{n: 1, at: -1, err: "invalid offset"},
-
-		{n: 0, at: 0, want: "01234567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 0, want: "1234567abcd!@#efghSTUVWXYZ"},
-		{n: 2, at: 0, want: "234567abcd!@#efghSTUVWXYZ"},
-		{n: 3, at: 0, want: "34567abcd!@#efghSTUVWXYZ"},
-		{n: 4, at: 0, want: "4567abcd!@#efghSTUVWXYZ"},
-		{n: 5, at: 0, want: "567abcd!@#efghSTUVWXYZ"},
-		{n: 6, at: 0, want: "67abcd!@#efghSTUVWXYZ"},
-		{n: 7, at: 0, want: "7abcd!@#efghSTUVWXYZ"},
-		{n: 8, at: 0, want: "abcd!@#efghSTUVWXYZ"},
-		{n: 9, at: 0, want: "bcd!@#efghSTUVWXYZ"},
-		{n: 26, at: 0, want: "Z"},
-		{n: 27, at: 0, want: ""},
-
-		{n: 0, at: 1, want: "01234567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 1, want: "0234567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 2, want: "0134567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 3, want: "0124567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 4, want: "0123567abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 5, want: "0123467abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 6, want: "0123457abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 7, want: "0123456abcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 8, want: "01234567bcd!@#efghSTUVWXYZ"},
-		{n: 1, at: 9, want: "01234567acd!@#efghSTUVWXYZ"},
-		{n: 8, at: 1, want: "0bcd!@#efghSTUVWXYZ"},
-		{n: 26, at: 1, want: "0"},
-		{n: 25, at: 1, want: "0Z"},
-	}
-	for _, test := range tests {
-		b := makeTestBytes(t)
-		defer b.Close()
-
-		err := b.Delete(test.n, test.at)
-		if !errMatch(test.err, err) {
-			t.Errorf("delete(%v, %v)=%v, want %v", test.n, test.at, err, test.err)
-			continue
-		}
-		if test.err != "" {
-			continue
-		}
-		if s := readAll(b); s != test.want || err != nil {
-			t.Errorf("%+v read failed: ReadAll(·)=%v,%v want %v,nil", test, s, err, test.want)
-		}
-	}
-}
-
-func TestBlockAlloc(t *testing.T) {
-	rs := []rune("αβξδφγθιζ")
-	l := len(rs)
-	if l <= testBlockSize {
-		t.Fatalf("len(rs)=%d, want >%d", l, testBlockSize)
-	}
-
-	b := NewBuffer(testBlockSize)
-	defer b.Close()
-
-	if err := b.Insert(rs, 0); err != nil {
-		t.Fatalf(`Initial insert(%v, 0)%v, wantnil`, rs, err)
-	}
-	if len(b.blocks) != 2 {
-		t.Fatalf("After initial insert: len(b.blocks)=%v, want 2", len(b.blocks))
-	}
-
-	if err := b.Delete(int64(l), 0); err != nil {
-		t.Fatalf(`Delete(%v, 0)=%v, want nil`, l, err)
-	}
-	if len(b.blocks) != 0 {
-		t.Fatalf("After delete: len(b.blocks)=%v, want 0", len(b.blocks))
-	}
-	if len(b.free) != 2 {
-		t.Fatalf("After delete: len(b.free)=%v, want 2", len(b.free))
-	}
-
-	rs = rs[:testBlockSize/2]
-	l = len(rs)
-
-	if err := b.Insert(rs, 0); err != nil {
-		t.Fatalf(`Second insert(%v, 7)=%v, want nil`, rs, err)
-	}
-	if len(b.blocks) != 1 {
-		t.Fatalf("After second insert: len(b.blocks)=%d, want 1", len(b.blocks))
-	}
-	if len(b.free) != 1 {
-		t.Fatalf("After second insert: len(b.free)=%d, want 1", len(b.free))
-	}
-}
-
-// TestInsertDeleteAndRead tests performing a few operations in sequence.
-func TestInsertDeleteAndRead(t *testing.T) {
-	b := NewBuffer(testBlockSize)
-	defer b.Close()
-
-	const hiWorld = "Hello, World!"
-	if err := b.Insert([]rune(hiWorld), 0); err != nil {
-		t.Fatalf(`insert(%s, 0)=%v, want nil`, hiWorld, err)
-	}
-	if s := readAll(b); s != hiWorld {
-		t.Fatalf(`readAll(·)=%v, want %s`, s, hiWorld)
-	}
-
-	if err := b.Delete(5, 7); err != nil {
-		t.Fatalf(`delete(5, 7)=%v, want nil`, err)
-	}
-	if s := readAll(b); s != "Hello, !" {
-		t.Fatalf(`readAll(·)=%v, want "Hello, !"`, s)
-	}
-
-	const gophers = "Gophers"
-	if err := b.Insert([]rune(gophers), 7); err != nil {
-		t.Fatalf(`insert(%s, 7)=%v, want nil`, gophers, err)
-	}
-	if s := readAll(b); s != "Hello, Gophers!" {
-		t.Fatalf(`readAll(·)=%v, want "Hello, Gophers!"`, s)
-	}
-}
-
-func errMatch(re string, err error) bool {
-	if err == nil {
-		return re == ""
-	}
-	if re == "" {
-		return err == nil
-	}
-	return regexp.MustCompile(re).Match([]byte(err.Error()))
-}
-
-func readAll(b *Buffer) string {
-	rs, err := b.Read(int(b.Size()), 0)
-	if err != nil {
-		panic(err)
-	}
-	return string(rs)
-}
-
-// Initializes a buffer with the text "01234567abcd!@#efghSTUVWXYZ"
-// split across blocks of sizes: 8, 4, 3, 4, 8.
-func makeTestBytes(t *testing.T) *Buffer {
-	b := NewBuffer(testBlockSize)
-	// Add 3 full blocks.
-	if err := b.Insert([]rune("01234567abcdefghSTUVWXYZ"), 0); err != nil {
-		b.Close()
-		t.Fatalf(`insert("01234567abcdefghSTUVWXYZ", 0)=%v, want nil`, err)
-	}
-	// Split block 1 in the middle.
-	if err := b.Insert([]rune("!@#"), 12); err != nil {
-		b.Close()
-		t.Fatalf(`insert("!@#", 12)=%v, want nil`, err)
-	}
-	ns := make([]int, len(b.blocks))
-	for i, blk := range b.blocks {
-		ns[i] = blk.n
-	}
-	if !reflect.DeepEqual(ns, []int{8, 4, 3, 4, 8}) {
-		b.Close()
-		t.Fatalf("blocks have sizes %v, want 8, 4, 3, 4, 8", ns)
-	}
-	return b
-}
-
-type errReadWriterAt struct{ error }
-
-func (e *errReadWriterAt) ReadAt([]byte, int64) (int, error)  { return 0, e.error }
-func (e *errReadWriterAt) WriteAt([]byte, int64) (int, error) { return 0, e.error }
-func (e *errReadWriterAt) Close() error                       { return e.error }
-
-// TestErrors tests some error cases. It's not exhaustive.
-func TestErrors(t *testing.T) {
-	str := []rune("Hello, World")
-	f := &errReadWriterAt{nil}
-	b := NewBufferReaderWriterAt(len(str)/2, f)
-
-	if err := b.Insert(str, 0); err != nil {
-		t.Fatalf("b.Insert(…)=%v, want nil", err)
-	}
-
-	// From here on, all IO causes an error.
-	f.error = errors.New("bad IO")
-
-	if _, err := b.Rune(0); err != f.error {
-		t.Errorf("b.Rune(0)=%v, want %v", err, f.error)
-	}
-	if err := b.Insert(str, 3); err != f.error {
-		t.Errorf("b.Insert(…)=%v, want %v", err, f.error)
-	}
-	if err := b.Delete(1, 0); err != f.error {
-		t.Errorf("b.Delete(…)=%v, want %v", err, f.error)
-	}
-	// The delete failed, so nothing should have been deleted.
-	if sz := b.Size(); sz != int64(len(str)) {
-		t.Errorf("b.Size()=%v, want %v", sz, len(str))
-	}
-	if _, err := b.Read(int(b.Size()), 0); err != f.error {
-		t.Errorf("b.Read(…)=%v, want %v", err, f.error)
-	}
-	if err := b.Close(); err != f.error {
-		t.Errorf("b.Close()=%v, want %v", err, f.error)
+	n, err := r.Read(make([]rune, 1))
+	if n != 0 || err != io.EOF {
+		t.Errorf("Read(len=1)=%d,%v, want 0,io.EOF", n, err)
 	}
 }
