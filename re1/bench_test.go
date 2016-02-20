52f953b7e2f96f9a5f751983aa50b1318dbec892
Ethan Burns <burns.ethan@gmail.com>
2016-02-20 08:32:06 -0500

    Revert "Delete re1."
    This reverts commit 20a444c4056ff2f9ab0098f326a3328626ea3b5d.

---
+++ re1/bench_test.go
@@ -0,0 +1,65 @@
+// Copyright © 2015, The T Authors.
+
+// Copied from go/test/bench/go1/regexp_test.go,
+// which requires the following notice:
+// Copyright 2013 The Go Authors. All rights reserved.
+
+package re1
+
+import (
+	"math/rand"
+	"strings"
+	"testing"
+)
+
+// benchmark based on regexp/exec_test.go
+
+var regexpText []byte
+
+func makeRegexpText(n int) []byte {
+	rand.Seed(0) // For reproducibility.
+	if len(regexpText) >= n {
+		return regexpText[:n]
+	}
+	regexpText = make([]byte, n)
+	for i := range regexpText {
+		if rand.Intn(30) == 0 {
+			regexpText[i] = '\n'
+		} else {
+			regexpText[i] = byte(rand.Intn(0x7E+1-0x20) + 0x20)
+		}
+	}
+	return regexpText
+}
+
+func benchmark(b *testing.B, re string, n int) {
+	r, err := Compile(strings.NewReader(re), Options{})
+	if err != nil {
+		panic(err)
+	}
+	t := makeRegexpText(n)
+	rs := sliceRunes([]rune(string(t)))
+	b.ResetTimer()
+	b.SetBytes(int64(n))
+	for i := 0; i < b.N; i++ {
+		if r.Match(rs, 0) != nil {
+			b.Fatal("match!")
+		}
+	}
+}
+
+const (
+	easy0  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+	easy1  = "A[AB]B[BC]C[CD]D[DE]E[EF]F[FG]G[GH]H[HI]I[IJ]J$"
+	medium = "[XYZ]ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+	hard   = "[ -~]*ABCDEFGHIJKLMNOPQRSTUVWXYZ$"
+)
+
+func BenchmarkRegexpMatchEasy0x32(b *testing.B)  { benchmark(b, easy0, 32<<0) }
+func BenchmarkRegexpMatchEasy0x1K(b *testing.B)  { benchmark(b, easy0, 1<<10) }
+func BenchmarkRegexpMatchEasy1x32(b *testing.B)  { benchmark(b, easy1, 32<<0) }
+func BenchmarkRegexpMatchEasy1x1K(b *testing.B)  { benchmark(b, easy1, 1<<10) }
+func BenchmarkRegexpMatchMediumx32(b *testing.B) { benchmark(b, medium, 1<<0) }
+func BenchmarkRegexpMatchMediumx1K(b *testing.B) { benchmark(b, medium, 1<<10) }
+func BenchmarkRegexpMatchHardx32(b *testing.B)   { benchmark(b, hard, 32<<0) }
+func BenchmarkRegexpMatchHardx1K(b *testing.B)   { benchmark(b, hard, 1<<10) }
---
+++ re1/delim_test.go
@@ -0,0 +1,193 @@
+package re1
+
+import "testing"
+
+func TestRemoveDelimiter(t *testing.T) {
+	tests := []struct {
+		name    string
+		in, out string
+	}{
+		{
+			name: "empty",
+			in:   "",
+			out:  "",
+		},
+		{
+			name: "no ending delimiter",
+			in:   "/abc",
+			out:  "abc",
+		},
+		{
+			name: "ending delimiter",
+			in:   "/abc/",
+			out:  "abc",
+		},
+		{
+			name: "escaped delimiter",
+			in:   `/ab\/c`,
+			out:  `ab/c`,
+		},
+		{
+			name: "escaped delimiter at start",
+			in:   `/\/abc`,
+			out:  `/abc`,
+		},
+		{
+			name: "escaped delimiter at end",
+			in:   `/abc\/`,
+			out:  `abc/`,
+		},
+		{
+			name: "charclass delimiter",
+			in:   `/ab[/]c`,
+			out:  `ab[/]c`,
+		},
+		{
+			name: "escaped and charclass delimiter",
+			in:   `/a\/b[/]c/`,
+			out:  `a/b[/]c`,
+		},
+		{
+			name: "non-ASCII delimiter",
+			in:   `☺abc☺`,
+			out:  `abc`,
+		},
+		{
+			name: "escaped and charclass non-ASCII delimiter",
+			in:   `☺a\☺b[☺]c☺`,
+			out:  `a☺b[☺]c`,
+		},
+		{
+			name: "[ in charclass",
+			in:   `/abc[[/]`,
+			out:  `abc[[/]`,
+		},
+		{
+			name: "escaped ] in charclass",
+			in:   `/abc[/\]]`,
+			out:  `abc[/\]]`,
+		},
+		{
+			name: "trailing escape",
+			in:   `/abc\`,
+			out:  `abc\`,
+		},
+	}
+	for _, test := range tests {
+		if _, str := RemoveDelimiter(test.in); str != test.out {
+			t.Errorf("%s (%q).String()=%q, want %q",
+				test.name, test.in, str, test.out)
+		}
+	}
+}
+
+func TestAddDelimiter(t *testing.T) {
+	tests := []struct {
+		name    string
+		in, out string
+		delim   rune
+	}{
+		{
+			name:  "empty",
+			in:    "",
+			delim: '/',
+			out:   "//",
+		},
+		{
+			name:  "simple",
+			in:    `abc`,
+			delim: '/',
+			out:   `/abc/`,
+		},
+		{
+			name:  "escape delimiter",
+			in:    `ab/c`,
+			delim: '/',
+			out:   `/ab\/c/`,
+		},
+		{
+			name:  "already escaped delimiter",
+			in:    `ab\/c`,
+			delim: '/',
+			out:   `/ab\/c/`,
+		},
+		{
+			name:  "charclass delimiter",
+			in:    `ab[/]c`,
+			delim: '/',
+			out:   `/ab[/]c/`,
+		},
+		{
+			name:  "escape meta delimiter",
+			in:    `abc*`,
+			delim: '*',
+			out:   `*abc\**`,
+		},
+		{
+			name:  "already escaped meta delimiter",
+			in:    `a\*`,
+			delim: '*',
+			out:   `*a[*]*`,
+		},
+		{
+			name:  "charclass meta delimiter",
+			in:    `abc\*`,
+			delim: '*',
+			out:   `*abc[*]*`,
+		},
+		{
+			name:  "obrace delimiter",
+			in:    `a[xyz]b`,
+			delim: '[',
+			out:   `[a\[xyz]b[`,
+		},
+		{
+			name:  "obrace delimiter add charclass",
+			in:    `a\[b`,
+			delim: '[',
+			out:   `[a[[]b[`,
+		},
+		{
+			name:  "trailing escape",
+			in:    `abc\`,
+			delim: '/',
+			out:   `/abc\\/`,
+		},
+		{
+			name:  "only escape",
+			in:    `\`,
+			delim: '/',
+			out:   `/\\/`,
+		},
+		{
+			name:  "escape in charclass",
+			in:    `[\]]`,
+			delim: '/',
+			out:   `/[\]]/`,
+		},
+		{
+			name:  "escaped delim in charclass",
+			in:    `[\/]`,
+			delim: '/',
+			out:   `/[\/]/`,
+		},
+		{
+			name:  "double escape",
+			in:    `\\[\\]`,
+			delim: '/',
+			out:   `/\\[\\]/`,
+		},
+		{
+			name:  "double escape before delimiter",
+			in:    `\\/`,
+			delim: '/',
+			out:   `/\\\//`,
+		},
+	}
+	for _, test := range tests {
+		if str := AddDelimiter(test.delim, test.in); str != test.out {
+			t.Errorf("%s (%q).DelimitedString(%q)=%q, want %q",
+				test.name, test.in, test.delim, str, test.out)
+		}
+	}
+}
